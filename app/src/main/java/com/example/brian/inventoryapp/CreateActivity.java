0d8d9a676ae8da6e68be96b34b145ddd646aaa6c
evansb <evansb326@gmail.com>
2017-02-07 18:23:26 -0600

    Declared all of the editText boxes

--- app/src/main/java/com/example/brian/inventoryapp/CreateActivity.java
+++ app/src/main/java/com/example/brian/inventoryapp/CreateActivity.java
@@ -5,17 +5,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import static com.example.brian.inventoryapp.R.id.editTextItem;
 
 public class CreateActivity extends AppCompatActivity {
 
+    EditText editTextItem;
+    EditText editTextModel;
+    EditText editTextSerial;
+    EditText editTextID;
     Button createButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
         getIntent();
 
+        editTextItem = (EditText)findViewById(R.id.editTextItem);
+        editTextModel = (EditText)findViewById(R.id.editTextModel);
+        editTextSerial = (EditText)findViewById(R.id.editTextSerial);
+        editTextID = (EditText)findViewById(R.id.editTextID);
         createButton = (Button) findViewById(R.id.createButton);
+
         createButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/brian/inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/brian/inventoryapp/MainActivity.java
@@ -5,16 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     Button addButton;
+    ListView itemListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getIntent();
-       addButton = (Button) findViewById(R.id.addButton);
-       addButton.setOnClickListener(new View.OnClickListener(){
+
+        itemListView = (ListView)findViewById(R.id.itemListView);
+        addButton = (Button) findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener(){
            @Override
            public void onClick(View v) {
                Intent intent = new Intent(MainActivity.this, CreateActivity.class);
