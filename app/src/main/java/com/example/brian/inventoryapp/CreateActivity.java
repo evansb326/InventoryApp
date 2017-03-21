bf6f9e0545e1a67713b7834d1c6794af6267eb1c
Black-Box <evansb326@gmail.com>
2017-03-21 16:56:04 -0500

    Can now scan barcodes for serial and model numbers.

--- app/src/main/java/com/example/brian/inventoryapp/CreateActivity.java
+++ app/src/main/java/com/example/brian/inventoryapp/CreateActivity.java
@@ -1,28 +1,37 @@
 package com.example.brian.inventoryapp;
 
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 
 public class CreateActivity extends AppCompatActivity {
 
+    private String id;
     EditText editTextItem;
     EditText editTextModel;
     EditText editTextSerial;
     EditText editTextID;
     Button saveButton;
+    ImageButton imageButtonModel;
+    ImageButton imageButtonSerial;
 
-    private String id;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
-        //getIntent();
+
+
 
         editTextItem = (EditText)findViewById(R.id.editTextItem);
         editTextModel = (EditText)findViewById(R.id.editTextModel);
@@ -30,9 +39,41 @@ public class CreateActivity extends AppCompatActivity {
         editTextID = (EditText)findViewById(R.id.editTextID);
         editTextID.setEnabled(false);
         saveButton = (Button) findViewById(R.id.saveButton);
+        imageButtonModel = (ImageButton)findViewById(R.id.imageButtonModel);
+        imageButtonSerial = (ImageButton)findViewById(R.id.imageButtonSerial);
+
+
+
+        imageButtonModel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(CreateActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+        imageButtonSerial.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(CreateActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+
+        });
+
+
 
         Intent intent = getIntent();
-
         id = intent.getStringExtra("id");
         if(id != null && Integer.parseInt(id) >= 0) {
             SqliteHelper database = new SqliteHelper(this.getApplicationContext());
@@ -67,7 +108,25 @@ public class CreateActivity extends AppCompatActivity {
 
     }
 
-    private boolean isModified() { return id != null && !id.isEmpty(); }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+            if (result.getContents() == null){
+                Log.d("CreateActivity", "Cancelled Scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }else {
+                Log.d("CreateActivity", "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_SHORT).show();
+                editTextSerial.setText(result.getContents());
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+
+   // private boolean isModified() { return id != null && !id.isEmpty(); }
 
 
 
